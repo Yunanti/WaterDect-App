86f7ff89ce1bc02d8613c3427e7464c29ae026dd
yunantimh <yunantimogah@gmail.com>
2022-06-29 23:33:10 +0700

    update gallery

--- src/components/pages/gallery/Form.tsx
+++ src/components/pages/gallery/Form.tsx
@@ -1,60 +1,60 @@
-import React from 'react'
 import { useDispatch } from 'react-redux';
 import { setSelect } from '../../redux/action';
 
-export default function Form() {
-
+export default function Form({setisFirstTimeRender}: any) {
     const dispatch = useDispatch();
   
     const handleChange = (e: any) => {
+      setisFirstTimeRender(true);
       dispatch(setSelect(e.target.value))
     }
   
     const provinsiList = [
-        "Pilih Provinsi...",
-        "Aceh",
-        "Bali",
-        "Bangka Belitung",
-        "Banten",
-        "Bengkulu",
-        "Jawa Tengah",
-        "Kalimantan Tengah",
-        "Sulawesi Tengah",
-        "Jawa timur",
-        "Kalimantan Timur",
-        "Nusa Tenggara Timur",
-        "Gorontalo",
-        "Daerah Khusus Ibukota Jakarta",
-        "Jambi",
-        "Lampung",
-        "Maluku",
-        "Sulawesi Utara",
-        "Sumatera Utara",
-        "Papua",
-        "Riau",
-        "Kepulauan Riau",
-        "Sulawesi Tenggara",
-        "Kalimantan Selatan",
-        "Sulawesi Selatan",
-        "Sumatera Selatan",
-        "Jawa Barat",
-        "Kalimantan Barat",
-        "Nusa tenggara Barat",
-        "Papua Barat",
-        "Sulawesi Barat",
-        "Sumatera Barat",
-        "Daerah Istimewa Yogyakarta",
-        "Kalimantan Utara",
-        "Maluku Utara"
+      "Aceh",
+      "Bali",
+      "Bangka Belitung",
+      "Banten",
+      "Bengkulu",
+      "Jawa Tengah",
+      "Kalimantan Tengah",
+      "Sulawesi Tengah",
+      "Jawa timur",
+      "Kalimantan Timur",
+      "Nusa Tenggara Timur",
+      "Gorontalo",
+      "Daerah Khusus Ibukota Jakarta",
+      "Jambi",
+      "Lampung",
+      "Maluku",
+      "Sulawesi Utara",
+      "Sumatera Utara",
+      "Papua",
+      "Riau",
+      "Kepulauan Riau",
+      "Sulawesi Tenggara",
+      "Kalimantan Selatan",
+      "Sulawesi Selatan",
+      "Sumatera Selatan",
+      "Jawa Barat",
+      "Kalimantan Barat",
+      "Nusa tenggara Barat",
+      "Papua Barat",
+      "Sulawesi Barat",
+      "Sumatera Barat",
+      "Daerah Istimewa Yogyakarta",
+      "Kalimantan Utara",
+      "Maluku Utara"
     ];
   
-    const provinsi = provinsiList.map((prov, index) =>
+    const provinsi = provinsiList.sort().map((prov, index) =>
       <option key={index} value={prov}>{prov}</option>
     );
   return (
     <>
         <form className='form-gallery'>
-          <select onChange={(e) => handleChange(e)}>
+          <select onChange={(e) => handleChange(e)} 
+          >
+            <option value={""}>Pilih Provinsi...</option>
             {provinsi}
           </select>
         </form>
