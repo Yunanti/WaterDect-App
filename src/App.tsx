81f782837e4d84e272472595c5a98d756875774a
yunantimh <yunantimogah@gmail.com>
2023-08-19 13:35:28 +0700

    update nav

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Provider } from 'react-redux';
 import store from './components/redux/store';
 import AppRoute from './routes/AppRoutes';
 import './styles/pages/About.css';
-import './styles/components/Navbar.css';
+import './styles/components/navbar.css';
 import './styles/pages/Home.css';
 import './styles/components/Form.css';
 import './styles/pages/Gallery.css';
